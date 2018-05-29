4a1d8e6bced10e4db9b6c0c05105c1aa476bb454
Prasanth <prasanth15sp@gmail.com>
2018-05-29 09:10:44 +0530

    Images Folder created and Login Backgroung Image Added

--- CreateAccount/src/createaccount/Login_form.java
+++ CreateAccount/src/createaccount/Login_form.java
@@ -41,18 +41,14 @@ public class Login_form extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        jLabel2 = new javax.swing.JLabel();
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 796, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 543, Short.MAX_VALUE)
-        );
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        getContentPane().setLayout(null);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/createaccount/Front Page.jpg"))); // NOI18N
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(1, -4, 1220, 760);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -93,5 +89,6 @@ public class Login_form extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
 }
