e9b240046d3ef13b2c49f70fbb7ca4e8aedce4c6
prasanth <prasanth15sp@gmail.com>
2018-06-05 11:55:11 +0530

    Welcome interface designing finished

--- CreateAccount/src/createaccount/Welcome.java
+++ CreateAccount/src/createaccount/Welcome.java
@@ -27,10 +27,28 @@ public class Welcome extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);
+
+        jButton1.setFont(new java.awt.Font("Arial Black", 1, 24)); // NOI18N
+        jButton1.setText("Turn on Face Recognition");
+        getContentPane().add(jButton1);
+        jButton1.setBounds(480, 250, 400, 40);
+
+        jButton2.setFont(new java.awt.Font("Arial Black", 1, 24)); // NOI18N
+        jButton2.setText("Updates");
+        getContentPane().add(jButton2);
+        jButton2.setBounds(600, 340, 150, 40);
+
+        jButton3.setFont(new java.awt.Font("Arial Black", 1, 24)); // NOI18N
+        jButton3.setText("Change Password");
+        getContentPane().add(jButton3);
+        jButton3.setBounds(530, 420, 290, 40);
 
         jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\prasanth\\Desktop\\Interectice Project\\WelcomePage.jpg")); // NOI18N
         getContentPane().add(jLabel1);
@@ -75,6 +93,9 @@ public class Welcome extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
