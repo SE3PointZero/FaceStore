3649ca9fd7accff12f3021ae907ee8af3b6259c6
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-07-23 17:49:29 +0530

    Necessary code added

--- CreateAccount/src/Classes/FullScreen.java
+++ CreateAccount/src/Classes/FullScreen.java
@@ -1,5 +1,36 @@
 package Classes;
 
+import java.awt.Toolkit;
+
 public class FullScreen {
     
+    private int width;
+    private int height;
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    
+    public FullScreen() {
+        
+    }
+    
+    public void SetResolution(){
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        
+        setWidth((int) tk.getScreenSize().getWidth());
+        setHeight((int) tk.getScreenSize().getHeight());
+    }
 }
