4f145589d3e535976909b24de1088c46945b0c27
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-08-25 11:34:24 +0530

    Take snap shot class created

--- CreateAccount/src/VisualBox/SnapShot.java
+++ CreateAccount/src/VisualBox/SnapShot.java
@@ -1,5 +1,28 @@
 package VisualBox;
 
+import org.opencv.core.*;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.videoio.VideoCapture;
+
 public class SnapShot {
     
+    public void TakeSnapShot(){
+        
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        VideoCapture camera = new VideoCapture(0);
+        
+        if(!camera.isOpened()){
+            System.out.println("Error");
+        }else{
+            Mat frame = new Mat();
+            while(true){
+                if(camera.read(frame)){
+                    System.out.println("Frame obtained");
+                    Imgcodecs.imwrite("camera.jpg",frame);
+                    break;
+                }
+            }
+        }
+        camera.release();
+    }
 }
