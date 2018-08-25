ce9d64c5c10de4b99723a868668b08334a65c195
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-08-25 12:45:52 +0530

    Welcome class buttons connected to other relevant interfaces

--- CreateAccount/src/createaccount/Welcome.java
+++ CreateAccount/src/createaccount/Welcome.java
@@ -24,21 +24,41 @@ public class Welcome extends javax.swing.JFrame {
 
         jButton1.setFont(new java.awt.Font("Arial Black", 1, 24)); // NOI18N
         jButton1.setText("Turn on Face Recognition");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton1);
         jButton1.setBounds(480, 270, 400, 40);
 
         jButton2.setFont(new java.awt.Font("Arial Black", 1, 24)); // NOI18N
         jButton2.setText("Updates");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton2);
         jButton2.setBounds(530, 390, 150, 40);
 
         jButton3.setFont(new java.awt.Font("Arial Black", 1, 24)); // NOI18N
         jButton3.setText("Change Password");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton3);
         jButton3.setBounds(530, 330, 290, 40);
 
         jButton4.setFont(new java.awt.Font("Arial Black", 1, 24)); // NOI18N
         jButton4.setText("Logout");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
         getContentPane().add(jButton4);
         jButton4.setBounds(690, 390, 129, 43);
 
@@ -48,6 +68,28 @@ public class Welcome extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        this.setVisible(false);
+        FaceRecognition objFaceRec = new FaceRecognition();
+        objFaceRec.setVisible(true);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        this.setVisible(false);
+        PasswordChanging objPassChange = new PasswordChanging();
+        objPassChange.setVisible(true);
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        this.setVisible(false);
+        Updates objupdate = new Updates();
+        objupdate.setVisible(true);
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        System.exit(0);
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     public static void main(String args[]) {
 
