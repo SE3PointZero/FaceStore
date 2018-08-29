9abefab6a1e2c1cbfdb991caf35196d9e9fd4b13
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-08-29 20:42:46 +0530

    Code added to VisualVariable class

--- CreateAccount/src/VisualBox/VisualVariables.java
+++ CreateAccount/src/VisualBox/VisualVariables.java
@@ -1,5 +1,19 @@
 package VisualBox;
 
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfRect;
+import org.opencv.videoio.VideoCapture;
+import org.opencv.objdetect.CascadeClassifier;
+
 public class VisualVariables {
+    
+    //private DaemonThread myThread = null;
+    int count = 0;
+    VideoCapture webSource = null;
+    Mat frame = new Mat();
+    MatOfByte mem = new MatOfByte();
+    CascadeClassifier faceDetector = new CascadeClassifier(VisualVariables.class.getResource("haarcascade_frontalface_alt.xml").getPath().substring(1));
+    MatOfRect faceDetections = new MatOfRect();
     
 }
