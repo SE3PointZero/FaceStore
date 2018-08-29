802c70831c3d22d207382c41f9f71645e1e55607
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-08-29 21:53:24 +0530

    Graphics class finished

--- CreateAccount/src/VisualBox/Graphics.java
+++ CreateAccount/src/VisualBox/Graphics.java
@@ -1,5 +1,13 @@
 package VisualBox;
 
+import javax.swing.JPanel;
+
 public class Graphics {
+
+    public Graphics(JPanel p) {
+        
+        java.awt.Graphics g = p.getGraphics();
+    
+    }
     
 }
