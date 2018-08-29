d7e518f2515d3ebdc5da15ad7b98d20352b28482
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-08-29 20:46:32 +0530

    Video class created and codes added

---
+++ CreateAccount/src/VisualBox/Video.java
@@ -0,0 +1,13 @@
+package VisualBox;
+
+import org.opencv.videoio.VideoCapture;
+
+public class Video {
+    
+//    webSource = new VideoCapture(0);
+//    myThread = new DaemonThread();
+//    Thread t = new Thread(myThread);
+//    t.setDaemon(true);
+//    myThread.runnable = true;
+//    t.start();
+}
