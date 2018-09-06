cca6a5698ac247d883d284a8e0edb142788df7ca
Prasanth <31370970+prasanthse@users.noreply.github.com>
2018-09-06 11:47:06 +0530

    DeleteCapturedImages class created and working successfull

--- CreateAccount/src/Classes/DeleteCapturedImages.java
+++ CreateAccount/src/Classes/DeleteCapturedImages.java
@@ -1,8 +1,20 @@
 package Classes;
 
+import java.io.File;
+
 public class DeleteCapturedImages {
     
+    File file;
+    private static final String workingDir = System.getProperty("user.dir");
+
+    public static String getWorkingDir() {
+        return workingDir;
+    }
+    
     public void deletePicOnProject(){
-        
+        for(int i=0; i<5; i++){
+            file = new File(getWorkingDir() + "\\" + "" + (i+1) + ".jpeg");
+            file.delete();
+        }
     }
 }
